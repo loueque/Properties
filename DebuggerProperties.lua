cd9bbb39376158c2c34cbbbe03f99ed702e46488
loueque <66234951+loueque@users.noreply.github.com>
2021-04-19 18:41:34 -0700

    Debugger, initialize()!
    Debugger for script debugging, or unwanted instances

---
+++ DebuggerProperties.lua
@@ -0,0 +1,35 @@
+local Properties = {}
+Properties.__index = {}
+
+function Properties:GetProperties(int: any)
+	local function r(i, p)
+		local t = table.create(#p)
+
+		for I,v in ipairs(p) do
+			t[I] = i[v]
+		end
+
+		return print(table.unpack(t)), print(table.getn(t))
+	end
+	
+	local function d(d)
+		local cd = table.create(#d)
+		
+		return table.unpack(d), d == nil
+	end
+
+  local function run(r)
+    local t = tick()
+    local tim = time()
+
+    while r do
+      print(t - tick() and tim or time())
+    end
+  end
+
+  if int:IsA("ScriptDebugger") then
+    ["Name"] = int.Name
+    
+    return int
+  end
+end
